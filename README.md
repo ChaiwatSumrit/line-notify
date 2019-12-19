856fc78cfcc6a6ad6f707a9d26c96fbbe8d054ef
chaiwatsumrit <chaiwat.sumrit@gmail.com>
2019-12-19 17:14:16 +0700

    add line@notify

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # line-notify
-line notify from api server node.js 
+line@notify from "api server node.js" to "Group line" for "Team develop"
+
+## line@notify yourself
+step by step
+- step 1 : login your line account to https://notify-bot.line.me
+- step 2 : in web "notify-bot" select menu go to "my-page"
+- step 3 : select botom "generate Token"
+- step 4 : input name bot line@notify and select end-point EX: Group line
+- step 5 : submit generate Token copy Token to note.txt and close 
+- step 6 : implement sample api server test shut down api and send notify by token your generate
+
+read more : https://www.chaichon.com/nodejs/line-notify-%E0%B8%94%E0%B9%89%E0%B8%A7%E0%B8%A2-nodejs/
+
+## How To Start
+clone repository
+```
+npm install
+npm start
+```
+use <CTRL+C> in terminal to kill process api
+api server will send notify to line@notify by token 
+and line@notify send notify to group line
+
---
+++ server.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const app = express()
+
+//test api is work
+app.get('/', (req, res) => {
+    res.send('Hi!')
+})
+
+const server = app.listen(3000, () => {
+    //token cs_BC
+    var token = "Xswf7uOXsbHGHwcPOZXcpUJUyWyU3xA7Z3dkTp36gsr";
+    //res notify to group line
+    var eventNotify = {
+        message: "[Test]💪API Server Online",
+        //package sticker : https://devdocs.line.me/files/sticker_list.pdf
+        //emoji : https://emojipedia.org/flexed-biceps/
+        stickerPackageId: 1,
+        stickerId: 403
+    }
+    lineNotify(token, eventNotify);
+    console.log('Server:3000 ready')
+})
+
+
+// seting process event
+
+// SIGTERM จะมีการส่งออกมาเมื่อ Node.js ของเรานั้นตายหรือไม่สามารถทำงานได้
+// SIGINT จะมีการส่งออกออกมาตอนที่เราหยุดการทำงานของ Server จาก terminal ด้วย CTRL+C
+
+process.on('SIGINT', () => {
+    server.close(() => {
+        //token cs_BC
+        var token = "Xswf7uOXsbHGHwcPOZXcpUJUyWyU3xA7Z3dkTp36gsr";
+        //res notify to group line
+        var eventNotify = {
+            message: "[Test]💥API Server Shutting Down!!! ",
+            //pack sticker https://devdocs.line.me/files/sticker_list.pdf
+            stickerPackageId: 1,
+            stickerId: 403
+        }
+        lineNotify(token, eventNotify);
+        console.log('Process terminated')
+    })
+})
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+        //token cs_BC
+        var token = "Xswf7uOXsbHGHwcPOZXcpUJUyWyU3xA7Z3dkTp36gsr";
+        //res notify to group line
+        var eventNotify = {
+            message: "[Test]💥API Server Shutting Down!!! ",
+            //pack sticker https://devdocs.line.me/files/sticker_list.pdf
+            stickerPackageId: 1,
+            stickerId: 403
+        }
+        lineNotify(token, eventNotify);
+        console.log('Process terminated')
+    })
+})
+
+//line notify event
+function lineNotify(token, eventNotify) {
+    //LINE@NOTIFY
+    console.log('Line notify to Team develop');
+    var request = require('request');
+
+    //set http request to line@notify 
+    //line@notify validate token add send event to group line
+    request({
+        method: 'POST',
+        uri: 'https://notify-api.line.me/api/notify',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            // 'Authorization': 'Bearer RmgbwNGiNZ8nu8y2Y6dMPujBRdsrTQWIZkkQ1LIuRGh'
+        },
+        auth: {
+            'bearer': token
+        },
+        form: eventNotify
+    }, (err, httpResponse, body) => {     //send http request to line@notify 
+        if (err) {
+            console.log(err);
+        } else {
+            res.json({
+                httpResponse: httpResponse,
+                body: body
+            });
+        }
+    });
+}
